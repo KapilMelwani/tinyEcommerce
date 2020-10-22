be68af2c46b5678b2d4393a738b6c90de4b7f41e
Kapil <kapil.melwani@codeoscopic.com>
2020-10-22 18:15:05 +0100

    refs develop customer service layer

--- src/main/java/com/mhp/solutions/tiny/ecommerce/services/ICustomerService.java
+++ src/main/java/com/mhp/solutions/tiny/ecommerce/services/ICustomerService.java
@@ -1,6 +1,12 @@
 package com.mhp.solutions.tiny.ecommerce.services;
 
+import com.mhp.solutions.tiny.ecommerce.entities.dto.CustomersDto;
+
 import java.util.List;
 
 public interface ICustomerService {
+    CustomersDto getAdministratorByIdAndActive(Long customerId, Boolean isActive);
+    void addCustomer(CustomersDto customersDto);
+    void updateCustomer(CustomersDto customersDto);
+    void deleteCustomerById(Long customerId);
 }
--- src/main/java/com/mhp/solutions/tiny/ecommerce/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/mhp/solutions/tiny/ecommerce/services/impl/CustomerServiceImpl.java
@@ -1,8 +1,46 @@
 package com.mhp.solutions.tiny.ecommerce.services.impl;
 
+import com.mhp.solutions.tiny.ecommerce.config.ObjectMapperUtils;
+import com.mhp.solutions.tiny.ecommerce.entities.Customers;
+import com.mhp.solutions.tiny.ecommerce.entities.dto.CustomersDto;
+import com.mhp.solutions.tiny.ecommerce.repository.CustomersRepo;
 import com.mhp.solutions.tiny.ecommerce.services.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CustomerServiceImpl implements ICustomerService {
+
+    @Autowired
+    CustomersRepo customersRepo;
+
+    @Override
+    public CustomersDto getAdministratorByIdAndActive(Long customerId, Boolean isActive) {
+        Customers entity = customersRepo.findCustomersByIdAndUserIsActive(customerId,isActive);
+        CustomersDto administatorsDto = ObjectMapperUtils.map(entity,CustomersDto.class);
+        return administatorsDto;
+    }
+
+    @Override
+    @Transactional
+    public void addCustomer(CustomersDto customersDto) {
+        final Customers entity = ObjectMapperUtils.map(customersDto, Customers.class);
+        customersRepo.save(entity);
+        customersDto.setId(entity.getId());
+    }
+
+    @Override
+    @Transactional
+    public void updateCustomer(CustomersDto customersDto) {
+        final Customers entity = ObjectMapperUtils.map(customersDto, Customers.class);
+        customersRepo.save(entity);
+        customersDto.setId(entity.getId());
+    }
+
+    @Override
+    @Transactional
+    public void deleteCustomerById(Long customerId) {
+        customersRepo.deleteCustomersById(customerId);
+    }
 }
